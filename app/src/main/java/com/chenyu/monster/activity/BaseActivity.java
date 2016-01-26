c8742f4eedc01e28170616737ebf8ef2e33235db
王晨宇 <wangchenyu100@gmail.com>
2016-01-26 15:47:53 +0800

    一血

---
+++ app/src/main/java/com/chenyu/monster/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.chenyu.monster.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by chenyu on 16/1/26.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
